8cf1a67654f23a3911e13d3a723b0b63f57246c2
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-02-16 16:24:24 +0300

    Решена задача

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива из строк,
+// длина которых меньше либо равна 3 символа.
+// Первоночальный массив можно ввесте с клавиатуры, либо задать на старте выполнения алгоритма
+
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+int len = 3;
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+
+ if (element.Length <= len)
+    {
+        array[i] = element;
+    }
+}
+
+Console.WriteLine();
+PrintArray(array);
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + " ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
